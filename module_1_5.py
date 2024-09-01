3e271397fcf6901e4adb53751444ede835613b38
Sv-Dobrova <sv-dobrova@yandex.ru>
2024-09-01 19:41:04 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 3, 'str', True
+print(immutable_var)
+#immutable_var [0]=10
+#print(immutable_var)
+mutable_list = [1, 2, 3, True, "String"]
+print (mutable_list)
+mutuble_list_2 = (1, 2, 3, 4)
+print(mutuble_list_2)
